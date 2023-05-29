c215018236d16d38e110c73428183a5d11b02793
Vishal-Ethinos <vishal.thakur@ethinos.com>
2023-05-29 16:29:47 +0530

    added home page banner and search bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
-import NavBar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import NavBar from "@/components/Navbar/navbar";
+import Footer from "@/components/Footer/footer";
 import { Raleway } from "next/font/google";
 
 const raleway = Raleway({ subsets: ["latin"] });
